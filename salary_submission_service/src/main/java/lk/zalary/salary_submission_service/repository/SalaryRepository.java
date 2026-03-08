31fe19770da64914e34541e0db08b4b9d763e7ab
samalchandrasiri <samalrandula87@gmail.com>
2026-03-08 20:15:22 +0530

    db scripts changes and class refactoring

--- salary_submission_service/src/main/java/lk/zalary/salary_submission_service/repository/SalaryRepository.java
+++ salary_submission_service/src/main/java/lk/zalary/salary_submission_service/repository/SalaryRepository.java
@@ -1,9 +1,9 @@
 package lk.zalary.salary_submission_service.repository;
 
-import lk.zalary.salary_submission_service.entity.Salary;
+import lk.zalary.salary_submission_service.entity.SalarySubmissions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SalaryRepository extends JpaRepository<Salary, Long> {
+public interface SalaryRepository extends JpaRepository<SalarySubmissions, Long> {
 }
