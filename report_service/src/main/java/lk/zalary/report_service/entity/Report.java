5fad7aa11b79cf0d233f7c4fac64dbedf573a704
Sharkyder <sahan.randula29@gmail.com>
2026-04-28 14:38:34 +0530

    fixed schema issues

--- report_service/src/main/java/lk/zalary/report_service/entity/Report.java
+++ report_service/src/main/java/lk/zalary/report_service/entity/Report.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "reports", schema = "zalary")
+@Table(name = "reports")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
